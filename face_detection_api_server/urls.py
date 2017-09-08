aaf0ab9100c16890c4ec33328f22f35c0407ac39
ShunyaUeta <shunyaueta@Shunya.local>
2017-09-08 16:26:46 +0900

    :lipstick: Add API urls patterns

--- face_detection_api_server/urls.py
+++ face_detection_api_server/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^face_detection/', include('face_detection.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include('face_detection_rest.urls', namespace='api')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
