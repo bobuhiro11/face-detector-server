5e5b0ce3c45317dbd053b8ffa35c3ef2a0e278cb
ShunyaUeta <shunyaueta@Shunya.local>
2017-09-08 16:49:59 +0900

    :lipstick: change image filename to UUID

--- face_detection/models.py
+++ face_detection/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+import uuid
+
 
 
 def scramble_uploaded_filename(instance, filename):
     extension = filename.split(".")[-1]
     return "{}.{}".format(uuid.uuid4(), extension)
 
+
 class UploadedImage(models.Model):
-    image = models.ImageField('Uploaded image')
+    image = models.ImageField("Uploaded image", upload_to=scramble_uploaded_filename)
